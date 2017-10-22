5d4b0bb276058689b08bd5fbd57ee496b1ad56e4
Vivek Joy <oneirolabs@Viveks-MacBook-Air.local>
2017-10-22 21:47:15 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router =  express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+// const jwt = require('jsonwebtoken');
+var jwt=require('jwt-simple');
+const config = require('../config/database');
+
+//Register
+
+router.post('/register',(req,res,next)=>{
+   let newUser = new User({
+       name:req.body.name,
+       email:req.body.email,
+       username:req.body.username,
+       password:req.body.password
+   });
+
+   User.addUser(newUser,(err,user)=>{
+       if(err){
+           res.json({success:false,msg:'Failed to register'});
+       }else{
+           res.json({success:true,msg:'User registered'});
+       }
+   })
+})
+
+//Authenticate
+
+router.post('/authenticate',(req,res,next)=>{
+   const username = req.body.username;
+   const password = req.body.password;
+
+   User.getUserByName(username,(err,user)=>{
+       if(err) throw err;
+       if(!user){
+           return res.json({success:false,msg:"User not found!"});
+       }
+
+       User.comparePassword(password,user.password,(err,isMatch)=>{
+           if(err) throw err;
+           if(isMatch){
+               console.log(user);
+            //    const token = jwt.sign({
+            //       exp:604800,
+            //       data:user
+            //    },config.secret);
+            const token  = jwt.encode({
+                id:user._id
+            },config.secret)
+
+               res.json({
+                   success:true,
+                   token:'JWT '+token,
+                   user:{
+                       id: user._id,
+                       name:user.username,
+                       email:user.email
+                   }
+               });
+           }else{
+               return res.json({success:false,msg:'Wrong password'});
+           }
+       })
+   })
+});
+
+//Profile
+
+router.get('/profile',passport.authenticate('jwt',{session:false}),(req,res,next)=>{
+    res.send({user: req.user});
+})
+
+module.exports = router;
